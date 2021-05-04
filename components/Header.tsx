e7d0f045b86aaa9776ae429a7d74be0f855b6428
鈴木孝伸 <>
2021-05-04 20:56:41 +0900

    feature/OGP

--- components/Header.tsx
+++ components/Header.tsx
@@ -32,7 +32,7 @@ export default function Header(props: Props): JSX.Element {
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
         <meta name="keywords" content={keyword} />
-        <meta property="og:type" content="blog" />
+        <meta property="og:type" content="website" />
         <meta property="og:url" content={url} />
         <meta property="og:image" content={image} />
         <meta property="og:site_name" content={title} />
