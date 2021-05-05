1bcedc264483ce9a83fe2a1cb45f0653e4937be0
鈴木孝伸 <>
2021-05-05 11:27:10 +0900

    feature/OGP

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,9 @@
 import React from "react";
 import "../styles/globals.css";
 import "semantic-ui-css/semantic.min.css";
-import Header from "../components/Header";
 
 function MyApp({ Component, pageProps }): JSX.Element {
-  return (
-    // <Header>
-    <Component {...pageProps} />
-    // </Header>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -21,34 +21,32 @@ export default function Home(): JSX.Element {
 
   if (isLoading) {
     return (
-      <Header>
-        <Dimmer active={true} inverted>
-          <Loader inline="centered" size="huge">
-            Loading
-          </Loader>
-        </Dimmer>
-      </Header>
+      <Dimmer active={true} inverted>
+        <Header />
+        <Loader inline="centered" size="huge">
+          Loading
+        </Loader>
+      </Dimmer>
     );
   }
 
   return (
-    <Header>
-      <div className={styles["home-wrapper"]}>
-        <h1 className={styles["home-wrapper__title"]}>
-          Current weather in Japan
-        </h1>
-        <div className={styles["home-wrapper__container"]}>
-          <div className={styles["home-wrapper__japan-map"]}>
-            <ul className={styles["cities"]}>
-              {cityCardInfo.map((city: City, Index: number) => {
-                return <CityCard city={city} key={Index} />;
-              })}
-            </ul>
-          </div>
+    <div className={styles["home-wrapper"]}>
+      <Header />
+      <h1 className={styles["home-wrapper__title"]}>
+        Current weather in Japan
+      </h1>
+      <div className={styles["home-wrapper__container"]}>
+        <div className={styles["home-wrapper__japan-map"]}>
+          <ul className={styles["cities"]}>
+            {cityCardInfo.map((city: City, Index: number) => {
+              return <CityCard city={city} key={Index} />;
+            })}
+          </ul>
         </div>
-        <CityList regions={japanRegions} citiesInRegion={japanCitiesInRegion} />
-        <Footer></Footer>
       </div>
-    </Header>
+      <CityList regions={japanRegions} citiesInRegion={japanCitiesInRegion} />
+      <Footer></Footer>
+    </div>
   );
 }
